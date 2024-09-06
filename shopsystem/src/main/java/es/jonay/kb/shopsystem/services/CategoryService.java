cc73deb6612dd614ed104265cda979ff44f68168
JonayKB <jonaykb@gmail.com>
2024-09-06 21:36:44 +0100

    Fixed

--- shopsystem/src/main/java/es/jonay/kb/shopsystem/services/CategoryService.java
+++ shopsystem/src/main/java/es/jonay/kb/shopsystem/services/CategoryService.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,7 +20,7 @@ import es.jonay.kb.shopsystem.api.dto.CategoryDto;
 import es.jonay.kb.shopsystem.api.dto.ItemDto;
 import es.jonay.kb.shopsystem.controller.CategoryController;
 import es.jonay.kb.shopsystem.controller.ItemController;
-import io.swagger.v3.oas.annotations.parameters.RequestBody;
+
 
 @RestController
 @RequestMapping("category")
--- shopsystem/src/main/java/es/jonay/kb/shopsystem/services/TradeService.java
+++ shopsystem/src/main/java/es/jonay/kb/shopsystem/services/TradeService.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,7 +20,6 @@ import es.jonay.kb.shopsystem.api.dto.ItemDto;
 import es.jonay.kb.shopsystem.api.dto.TradeDto;
 import es.jonay.kb.shopsystem.controller.ItemController;
 import es.jonay.kb.shopsystem.controller.TradeController;
-import io.swagger.v3.oas.annotations.parameters.RequestBody;
 
 @RestController
 @RequestMapping("trade")
